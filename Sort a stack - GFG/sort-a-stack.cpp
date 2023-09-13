e8c82806576349d5c0f4086e013de725b2f4d9e4
Maaz Khan <103138384+maazkhan101@users.noreply.github.com>
2023-09-13 14:04:37 +0530

    Added solution - LeetHub

---
+++ Sort a stack - GFG/sort-a-stack.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort()
+{
+   //Your code here
+   vector<int>a;
+   while(!s.empty())
+   {
+       a.push_back(s.top());
+       s.pop();
+   }
+   
+   
+   
+   
+   for(int i=0;i<a.size();i++)
+   {
+       int temp;
+       for(int j=i+1;j<a.size();j++)
+       {
+           if(a[i]>a[j])
+           {
+               temp=a[i];
+               a[i]=a[j];
+               a[j]=temp;
+           }
+       }
+   }
+   for(int i=0;i<a.size();i++)
+   {
+       s.push(a[i]);
+   }
+   
+}
