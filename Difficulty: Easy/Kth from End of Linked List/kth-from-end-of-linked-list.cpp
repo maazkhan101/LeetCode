c2a44a4b497b2bdec684accae127c0812ab065d4
Maaz Khan <103138384+maazkhan101@users.noreply.github.com>
2024-07-17 23:22:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+// Initial Template for C++
+//  C program to find n'th Node in linked list
+#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to get the kth node from the last of a linked list*/
+int getKthFromLast(struct Node *head, int k);
+
+/* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+// Function to find the data of kth node from the end of a linked list.
+class Solution {
+  public:
+    int getKthFromLast(Node *head, int k) {
+        // Your code here
+        
+        int ans=-1;
+        
+        vector<int>v;
+        Node *temp=head;
+        
+        while(temp!=NULL)
+        {
+            v.push_back(temp->data);
+            temp=temp->next;
+        }
+        
+        if(v.size()>=k)
+        {
+            ans=v[v.size()-k];
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T, i, n, l, k;
+
+    cin >> T;
+
+    while (T--) {
+        struct Node *head = NULL, *tail = NULL;
+
+        cin >> n >> k;
+        int firstdata;
+        cin >> firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for (i = 1; i < n; i++) {
+            cin >> l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        Solution obj;
+        cout << obj.getKthFromLast(head, k) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
