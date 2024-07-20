ae28c2368f42f69f6cc2153b487408dfb384915f
Maaz Khan <103138384+maazkhan101@users.noreply.github.com>
2024-07-20 13:13:28 +0530

    Time: 15 ms (57.45%) | Memory: 12.3 MB (45.58%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+         string t;
+
+        stack<char>st;
+
+       for (int i = 0; i <= s.length(); i++) {
+            if (i < s.length() && s[i] != ' ') {
+                st.push(s[i]);
+            } else {
+                while (!st.empty()) {
+                    t.push_back(st.top());
+                    st.pop();
+                }
+                if (i < s.length()) { // Add space only if it's not the end of the string
+                    t.push_back(' ');
+                }
+            }
+        }
+        
+        return t;
+    }
+};
