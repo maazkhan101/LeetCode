ab425d6b21d29c2284744926397e3db8c540488d
Maaz Khan <103138384+maazkhan101@users.noreply.github.com>
2025-10-09 16:33:44 +0530

    Time: 56 ms (5.55%) | Memory: 54.2 MB (21.64%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    
+    return{
+    toBe:function(val2)
+    {
+        if(val2===val)
+        {
+            return true;
+        }
+        throw new Error("Not Equal");
+    },
+    notToBe:function(val3)
+    {
+        if(val3!==val)
+        {
+            return true;
+        }
+        throw new Error("Equal");
+    }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
