aa68087ae98199ba93be1c5d0d2bd3313b72882b
Maaz Khan <103138384+maazkhan101@users.noreply.github.com>
2026-04-12 21:14:21 +0530

    Time: 1266 ms (5.74%) | Memory: 295.6 MB (5.25%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
-                sort(nums.begin(),nums.end());
-
-                set<vector<int>>s;
-
-                vector<vector<int>>ans;
-
-
-                int target=0;
-
-                for(int i=0;i<nums.size();i++)
+        
+        sort(nums.begin(),nums.end());
+        set<vector<int>>s;
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            int j=i+1;
+            int k=nums.size()-1;
+            vector<int>temp;
+            while(j<k)
+            {
+                int sum= nums[i] + nums[j] + nums[k];
+                if(sum==0)
                 {
-                    int j=i+1;
-                    int k=nums.size()-1;
-
-                    while(j<k)
-                    {
-                        int sum=nums[i]+nums[j]+nums[k];
-                        if(sum==target)
-                        {
-                            s.insert({nums[i],nums[j],nums[k]});
-                        }
-                        if(sum<target)
-                        {
-                            j++;
-                        }
-                        else{
-                            k--;
-                        }
-
-                    }
-                }  
-
-                for(auto i:s)
+                    s.insert({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    
+                }
+                if(sum<0)
                 {
-                    ans.push_back(i);
-                } 
+                    j++;
+                }
+                if(sum>0)
+                {
+                    k--;
+                }
+                
+                
 
-                return ans;
+            }
+        }
+            for(auto i:s)
+            {
+                ans.push_back(i);
+            }
 
+            
+        
+        return ans;
     }
 };
