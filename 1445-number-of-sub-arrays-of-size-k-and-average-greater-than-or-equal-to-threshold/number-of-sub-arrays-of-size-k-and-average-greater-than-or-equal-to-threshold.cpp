94dfd56c7710b6dd16e8c081381b84837a270beb
Maaz Khan <103138384+maazkhan101@users.noreply.github.com>
2026-04-29 20:36:48 +0530

    Time: 3 ms (26.12%) | Memory: 59.2 MB (35.32%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    int numOfSubarrays(vector<int>& nums, int k, int threshold) {
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
         int count=0;
-
-        int start=0;
-        int end=0;
-
+        int i=0;
+        int j=0;
         int sum=0;
 
-        int n=nums.size();
-
-        for(int i=0;i<k;i++)
+        while(j<arr.size())
         {
-            sum+=nums[i];
-         
-        }
-         int avg=sum/k;
-            if(avg>=threshold)
+            sum=sum+arr[j];
+            int avg=0;
+            if(j-i+1==k)
             {
-                count++;
+                avg=sum/k;
+                if(avg>=threshold)
+                {
+                    count++;
+                }
+                sum=sum-arr[i];
+                i++;
             }
-        for(int j=1;j<n-k+1;j++)
-        {
-            int prevele=nums[j-1];
-            int nextele=nums[j+k-1];
-            sum=sum+nextele-prevele;
-                int avg=sum/k;
-            if(avg>=threshold)
-            {
-                count++;
-            }
-        }
+            j++;
 
+            
+        }
         return count;
     }
 };
